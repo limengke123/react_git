c9058ed8454dd4f394a1b526ed0249c7db9b205e
limengke123 <752140235@qq.com>
2017-03-07 14:14:59 +0800

    添加文件

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 /**
  * Created by 75214 on 2017/3/7.
  */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render((
+    <div>Hello React!</div>
+), document.getElementById('app'));
